5111e5220221f99fa4f17082df6b529ed62c6a44
KevinSRDev <kevin.s.romero.r06@gmail.com>
2025-07-24 15:49:15 -0500

    Primer commit

---
+++ js/home/home.js
@@ -0,0 +1,43 @@
+const openButton = document.getElementById('open-SideBar-btn')
+const navbar = document.getElementById('navBarM')
+const media = window.matchMedia("(width < 600px)")
+
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e) {
+    const isMobile = e.matches
+    console.log(isMobile)
+
+    if (isMobile) {
+        navbar.setAttribute('inert', '')
+    }
+    else {
+        // desktop device
+        navbar.removeAttribute('inert')
+    }
+}
+
+function openSideBar() {
+    navbar.classList.add('show')
+    openButton.setAttribute('aria-expanded', 'true')
+    navbar.removeAttribute('inert')
+}
+
+function closeSideBar() {
+    navbar.classList.remove('show')
+    openButton.setAttribute('aria-expanded', 'false')
+    navbar.setAttribute('inert', '')
+}
+
+
+// Inavilita la Nav al dar el primer click :: hay que solucionar
+
+// const navLinks = document.querySelectorAll('nav a')
+// navLinks.forEach(Link => {
+//     Link.addEventListener('click', () => {
+//         closeSideBar()
+//     })
+// })
+
+updateNavbar(media)
