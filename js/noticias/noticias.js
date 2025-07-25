6957a7e898fbae953ae0b36097e621837ab1cb61
KevinSRDev <kevin.s.romero.r06@gmail.com>
2025-07-25 14:19:27 -0500

    Se añadio la importacion del Booststrap a las vista faltantes

--- js/noticias/noticias.js
+++ js/noticias/noticias.js
@@ -3,5 +3,5 @@ const botonLinkNew = document.getElementById('btn-link-new')
 botonLinkNew.addEventListener('click', linkNoticia)
 
 function linkNoticia(){
-   window.location.href = './pages/viewNoticia.html'
+   window.location.href = '../pages/viewNoticia.html'
 }
